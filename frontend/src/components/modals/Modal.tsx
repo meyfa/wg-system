e085da4452c2101bc0d89e53628fc00c55c3d913
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-09-29 13:43:27 +0200

    feat(components): Lock body scrolling when Modal is open

--- frontend/src/components/modals/Modal.tsx
+++ frontend/src/components/modals/Modal.tsx
@@ -1,18 +1,22 @@
 import './Modal.css'
-import { PropsWithChildren, ReactElement } from 'react'
+import { PropsWithChildren, ReactElement, useRef } from 'react'
 import { createPortal } from 'react-dom'
 import clsx from 'clsx'
+import { useScrollLock } from '../../util/use-scroll-lock'
 
 interface Props {
   active: boolean
 }
 
 export default function Modal (props: PropsWithChildren<Props>): ReactElement {
+  const ref = useRef<HTMLDivElement>(null)
+  useScrollLock(ref, props.active)
+
   if (!props.active) {
     return <></>
   }
 
-  return createPortal(<div className={clsx('Modal', { active: props.active })}>
+  return createPortal(<div ref={ref} className={clsx('Modal', { active: props.active })}>
     <div className='Modal-container'>
       {props.children}
     </div>
