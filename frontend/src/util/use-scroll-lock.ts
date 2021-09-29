e3034322f4df8b3127b643b104418ccfa7fd852c
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-09-29 13:52:25 +0200

    feat(components): Lock body scrolling when Modal is open (#31)

---
+++ frontend/src/util/use-scroll-lock.ts
@@ -0,0 +1,24 @@
+import { RefObject, useEffect } from 'react'
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+
+/**
+ * A custom hook that disables body scrolling. The provided reference is for allowing scroll inside a chosen
+ * foreground element, such as a modal. Only when the ref is set will the hook be able to work.
+ *
+ * @param ref The element ref.
+ * @param active Whether the scroll lock should be active.
+ */
+export function useScrollLock (ref: RefObject<HTMLElement>, active: boolean): void {
+  useEffect(() => {
+    if (ref.current != null) {
+      const element = ref.current
+      if (active) {
+        disableBodyScroll(element, { reserveScrollBarGap: true })
+        // cleanup function
+        return () => enableBodyScroll(element)
+      } else {
+        enableBodyScroll(element)
+      }
+    }
+  }, [ref, active])
+}
