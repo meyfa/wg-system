6da1725c56a5dd92ed63ffab6156d0c28965e9d3
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-09-29 17:33:17 +0200

    fix(api): Remove __v (Mongoose version key) from responses (#32)

--- backend/index.ts
+++ backend/index.ts
@@ -4,5 +4,9 @@ import { Environment } from './environment'
 export { createApiRouter } from './api/api'
 
 export async function init (env: Environment): Promise<void> {
+  mongoose.set('toJSON', {
+    versionKey: false
+  })
+
   await mongoose.connect(env.MONGODB_URI ?? 'mongodb://localhost/wgsystem')
 }
