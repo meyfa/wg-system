4a2748d2c2817d8cf757b887ec8648ebd3fb2e01
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-01 13:17:10 +0200

    feat: Indicate disconnected status via a modal
    When the socket is disconnected for over 3 seconds, a modal will be
    shown until connection is restored. This prevents the user from
    performing actions that might fail during that time.

---
+++ frontend/src/util/use-connection-status.ts
@@ -0,0 +1,51 @@
+import socket from '../websocket/socket'
+import { useEffect, useState } from 'react'
+
+/**
+ * A custom hook that provides the WebSocket connection status.
+ *
+ * @returns Whether the socket is currently connected.
+ */
+export function useConnectionStatus (): boolean {
+  const [connected, setConnected] = useState(socket.connected)
+
+  useEffect(() => {
+    const connectListener = (): void => setConnected(true)
+    const disconnectListener = (): void => setConnected(false)
+    socket.on('connect', connectListener)
+    socket.on('disconnect', disconnectListener)
+    return () => {
+      socket.off('connect', connectListener)
+      socket.off('disconnect', disconnectListener)
+    }
+  }, [])
+
+  return connected
+}
+
+/**
+ * A custom hook that provides the WebSocket connection status, but with a specified "grace period" for disconnected
+ * state. The hook will continue to indicate the connected state for the given delay, and only if that delay is over
+ * will it switch to disconnected (unless the connection was restored).
+ *
+ * @param delay The amount of time the socket can be disconnected while still showing as connected.
+ * @returns Whether the socket is currently connected or was connected recently enough.
+ */
+export function useConnectionStatusDebounced (delay: number): boolean {
+  const [wasConnected, setWasConnected] = useState(true)
+
+  const connected = useConnectionStatus()
+
+  useEffect(() => {
+    // propagate restored connection immediately
+    if (connected) {
+      setWasConnected(true)
+      return
+    }
+    // apply a delay to disconnect propagation
+    const timeout = setTimeout(() => setWasConnected(false), delay)
+    return () => clearTimeout(timeout)
+  }, [connected, delay])
+
+  return wasConnected
+}
