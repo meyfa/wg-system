fbce5a033a3a97ba496cabde4a9a2305b89ad3a5
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-01 23:03:33 +0200

    refactor(components): Implement EditableItem base component

--- frontend/src/components/garbage/ManualChoreItem.tsx
+++ frontend/src/components/garbage/ManualChoreItem.tsx
@@ -1,20 +1,14 @@
-import './ManualChoreItem.css'
 import { ManualChore } from '../../store/entities/manual-chores'
 import { ReactElement, useCallback, useState } from 'react'
-import BasicButton from '../forms/BasicButton'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
-import { useTranslation } from 'react-i18next'
 import api from '../../api/api'
 import EditManualChoreModal from './EditManualChoreModal'
+import EditableItem from '../items/EditableItem'
 
 interface Props {
   chore: ManualChore
 }
 
 export default function ManualChoreItem (props: Props): ReactElement {
-  const { t } = useTranslation()
-
   const [editing, setEditing] = useState(false)
 
   const showEditModal = useCallback(() => setEditing(true), [])
@@ -26,17 +20,8 @@ export default function ManualChoreItem (props: Props): ReactElement {
   }, [])
 
   return (
-    <div className='ManualChoreItem'>
-      <div className='ManualChoreItem-name'>
-        {props.chore.name}
-      </div>
-      <div className='ManualChoreItem-actions'>
-        <BasicButton onClick={showEditModal}>
-          <FontAwesomeIcon icon={faEdit} />
-          {t('basicActions.edit')}
-        </BasicButton>
-      </div>
+    <EditableItem itemName={props.chore.name} onClickEdit={showEditModal}>
       <EditManualChoreModal chore={props.chore} active={editing} onSave={save} onCancel={hideEditModal} />
-    </div>
+    </EditableItem>
   )
 }
