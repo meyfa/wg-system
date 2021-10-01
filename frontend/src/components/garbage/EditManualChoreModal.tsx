a27d5b73b84db0f96133fa614cabe2609db8d03e
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-01 18:39:04 +0200

    refactor(components): Implement EditModal base component (#38)

--- frontend/src/components/garbage/EditManualChoreModal.tsx
+++ frontend/src/components/garbage/EditManualChoreModal.tsx
@@ -1,12 +1,9 @@
 import './EditManualChoreModal.css'
 import { ReactElement, useCallback, useEffect, useMemo, useState } from 'react'
-import Modal from '../modals/Modal'
-import BasicButton from '../forms/BasicButton'
 import { useTranslation } from 'react-i18next'
 import TextField from '../forms/TextField'
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ManualChore } from '../../store/entities/manual-chores'
+import EditModal from '../modals/EditModal'
 
 interface Props {
   active: boolean
@@ -42,24 +39,16 @@ export default function EditManualChoreModal (props: Props): ReactElement {
   }, [onSave, props.chore, isValid, name, dueSince])
 
   return (
-    <Modal active={props.active}>
-      <div className='EditManualChoreModal-title'>
-        <FontAwesomeIcon icon={faEdit} />
-        {props.chore != null ? t('garbage.edit') : t('garbage.create')}
-      </div>
+    <EditModal title={props.chore != null ? t('garbage.edit') : t('garbage.create')}
+               active={props.active}
+               isValid={isValid}
+               onSave={save}
+               onCancel={props.onCancel}>
       <TextField
         value={name}
         placeholder={t('garbage.fields.name')}
         onChange={({ target }) => setName(target.value)}
       />
-      <div className='EditManualChoreModal-actions'>
-        <BasicButton onClick={save} disabled={!isValid}>
-          {t('basicActions.save')}
-        </BasicButton>
-        <BasicButton onClick={props.onCancel}>
-          {t('basicActions.cancel')}
-        </BasicButton>
-      </div>
-    </Modal>
+    </EditModal>
   )
 }
---
+++ frontend/src/components/modals/EditModal.tsx
@@ -0,0 +1,37 @@
+import './EditModal.css'
+import { PropsWithChildren, ReactElement } from 'react'
+import Modal from '../modals/Modal'
+import BasicButton from '../forms/BasicButton'
+import { useTranslation } from 'react-i18next'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+interface Props {
+  active: boolean
+  title: string
+  isValid: boolean
+  onSave: () => void
+  onCancel: () => void
+}
+
+export default function EditModal (props: PropsWithChildren<Props>): ReactElement {
+  const { t } = useTranslation()
+
+  return (
+    <Modal active={props.active}>
+      <div className='EditModal-title'>
+        <FontAwesomeIcon icon={faEdit} />
+        {props.title}
+      </div>
+      {props.children}
+      <div className='EditModal-actions'>
+        <BasicButton onClick={props.onSave} disabled={!props.isValid}>
+          {t('basicActions.save')}
+        </BasicButton>
+        <BasicButton onClick={props.onCancel}>
+          {t('basicActions.cancel')}
+        </BasicButton>
+      </div>
+    </Modal>
+  )
+}
