fdd0ff0bb29286ad0a58b54cd75fe06f69fb0ee2
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-03 21:42:53 +0200

    feat(pages): Remove title from Home page for more compact display

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,4 @@
 import { ReactElement } from 'react'
-import { useTranslation } from 'react-i18next'
-import PageTitle from '../components/PageTitle'
 import NavigationBarLayout from '../layouts/NavigationBarLayout'
 import { useAppSelector } from '../store/store'
 import { selectManualChores } from '../store/entities/manual-chores'
@@ -9,14 +7,11 @@ import { selectScoreboards } from '../store/entities/scoreboards'
 import ScoreboardBox from '../components/home/ScoreboardBox'
 
 export default function HomePage (): ReactElement {
-  const { t } = useTranslation()
-
   const scoreboards = useAppSelector(selectScoreboards)
   const manualChores = useAppSelector(selectManualChores)
 
   return (
     <NavigationBarLayout>
-      <PageTitle title={t('home.title')} />
       {scoreboards.map(scoreboard => <ScoreboardBox key={scoreboard._id} scoreboard={scoreboard} />)}
       {manualChores.map(chore => <ManualChoreBox key={chore._id} chore={chore} />)}
     </NavigationBarLayout>
