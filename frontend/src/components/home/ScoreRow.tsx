efba83f743beb0a738af0f5c5bab874bd77c95a2
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-03 21:45:40 +0200

    feat(components): Display relativeScore as number in ScoreRow
    Additionally, avoids a divide-by-zero problem when calculating badness.

--- frontend/src/components/home/ScoreRow.tsx
+++ frontend/src/components/home/ScoreRow.tsx
@@ -16,13 +16,17 @@ export default function ScoreRow (props: Props): ReactElement {
     return members.find(item => item._id === props.memberId)
   }, [members, props.memberId])
 
-  const badness = Math.abs(props.relativeScore / props.lowestScore)
+  const badness = props.lowestScore !== 0
+    ? Math.abs(props.relativeScore / props.lowestScore)
+    : 0
 
   return (
     <div className='ScoreRow'>
       <div className='ScoreRow-name'>{member?.name}</div>
       <div className='ScoreRow-bar'>
-        <span className='ScoreRow-bar-inner' style={{ width: (badness * 100).toFixed(0) + '%' }} />
+        <span className='ScoreRow-bar-inner' style={{ width: (badness * 100).toFixed(0) + '%' }}>
+          {props.relativeScore}
+        </span>
       </div>
     </div>
   )
