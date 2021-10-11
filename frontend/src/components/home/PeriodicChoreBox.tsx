a083b36e9b848b1299f0eb36797d4872c1caae34
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-11 11:28:16 +0200

    feat(pages): Show periodic chores on Home page (#60)
    * feat(pages): Show periodic chores on Home page
    
    * refactor(components): Split PeriodicChoreBox constructor into parts

---
+++ frontend/src/components/home/PeriodicChoreBox.tsx
@@ -0,0 +1,73 @@
+import './PeriodicChoreBox.css'
+import { ReactElement, useCallback, useState } from 'react'
+import ChoreBox from './ChoreBox'
+import { PeriodicChore, PeriodicChoreEntry } from '../../store/entities/periodic-chores'
+import { useEntityById } from '../../util/use-entity-by-id'
+import { Member, selectMembers } from '../../store/entities/members'
+import { useTranslation } from 'react-i18next'
+import BasicButton from '../forms/BasicButton'
+import api from '../../api/api'
+import { SelectMemberModal } from './SelectMemberModal'
+
+function useRecentlyCompletedString (entries: readonly PeriodicChoreEntry[]): string {
+  const last = entries.length > 0 ? entries[entries.length - 1] : undefined
+
+  const lastMember = useEntityById(selectMembers, last?.memberId)
+  // get only date part from ISO date-time string
+  const lastDate = last != null && last.date.length >= 10 ? last.date.substring(0, 10) : '???'
+
+  return last != null && lastMember != null ? `${lastMember.name}, ${lastDate}` : '--'
+}
+
+async function addEntry (chore: PeriodicChore, member: Member): Promise<void> {
+  await api.periodicChores.update({
+    ...chore,
+    entries: [
+      ...chore.entries,
+      {
+        memberId: member._id,
+        date: new Date().toISOString()
+      }
+    ]
+  })
+}
+
+interface Props {
+  chore: PeriodicChore
+}
+
+export default function PeriodicChoreBox (props: Props): ReactElement {
+  const { t } = useTranslation()
+
+  const recentlyCompleted = useRecentlyCompletedString(props.chore.entries)
+
+  const [isSelectingMember, setSelectingMember] = useState(false)
+
+  const startMarkDone = useCallback(() => setSelectingMember(true), [])
+  const cancelMarkDone = useCallback(() => setSelectingMember(false), [])
+
+  const confirmMarkDone = useCallback(async (member?: Member) => {
+    setSelectingMember(false)
+    if (member != null) {
+      await addEntry(props.chore, member)
+    }
+  }, [props.chore])
+
+  return (
+    <ChoreBox className='PeriodicChoreBox'>
+      <div className='PeriodicChoreBox-title'>
+        {props.chore.name}
+      </div>
+      <div className='PeriodicChoreBox-detail'>
+        {t('home.chores.last')}<br />
+        {recentlyCompleted}
+      </div>
+      <div>
+        <BasicButton onClick={startMarkDone}>
+          {t('home.chores.markDone')}
+        </BasicButton>
+      </div>
+      <SelectMemberModal active={isSelectingMember} onSelect={confirmMarkDone} onCancel={cancelMarkDone} />
+    </ChoreBox>
+  )
+}
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -5,13 +5,17 @@ import { selectManualChores } from '../store/entities/manual-chores'
 import ManualChoreBox from '../components/home/ManualChoreBox'
 import { selectScoreboards } from '../store/entities/scoreboards'
 import ScoreboardBox from '../components/home/ScoreboardBox'
+import { selectPeriodicChores } from '../store/entities/periodic-chores'
+import PeriodicChoreBox from '../components/home/PeriodicChoreBox'
 
 export default function HomePage (): ReactElement {
+  const periodicChores = useAppSelector(selectPeriodicChores)
   const scoreboards = useAppSelector(selectScoreboards)
   const manualChores = useAppSelector(selectManualChores)
 
   return (
     <NavigationBarLayout>
+      {periodicChores.map(chore => <PeriodicChoreBox key={chore._id} chore={chore} />)}
       {scoreboards.map(scoreboard => <ScoreboardBox key={scoreboard._id} scoreboard={scoreboard} />)}
       {manualChores.map(chore => <ManualChoreBox key={chore._id} chore={chore} />)}
     </NavigationBarLayout>
