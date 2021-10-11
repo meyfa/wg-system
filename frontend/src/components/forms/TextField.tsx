1e79dab76a4deac1ae837f63c28ac364ad5bbc76
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-11 11:14:31 +0200

    feat(pages): Implement creating/editing of periodic chores via UI

--- frontend/src/components/forms/TextField.tsx
+++ frontend/src/components/forms/TextField.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx'
 
 interface Props {
   value?: string | number
+  numeric?: boolean
   placeholder?: string
   onChange?: ChangeEventHandler<HTMLInputElement>
   className?: string
@@ -13,7 +14,7 @@ interface Props {
 export default function TextField (props: Props): JSX.Element {
   return (
     <input
-      type='text'
+      type={props.numeric === true ? 'number' : 'text'}
       className={clsx('TextField', props.className)}
       disabled={props.disabled}
       value={props.value}
