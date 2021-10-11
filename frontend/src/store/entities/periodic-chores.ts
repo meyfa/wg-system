6f2ac7e8477f0dcb776ad6928b237e94de2a2e8d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-11 11:09:29 +0200

    feat(models,api): Add PeriodicChore model and routes (#58)
    This type of chore can be used to set up a recurring chore with a fixed
    interval (measured in days). Completion is recorded as a tuple of the
    member who did the chore, and the respective date.

---
+++ frontend/src/store/entities/periodic-chores.ts
@@ -0,0 +1,22 @@
+import { Selector } from '@reduxjs/toolkit'
+import { createEntitySlice } from '../create-entity-slice'
+import { RootState } from '../store'
+import { Entity } from '../entity'
+
+export interface PeriodicChoreEntry {
+  readonly memberId: string
+  readonly date: string
+}
+
+export interface PeriodicChore extends Entity {
+  readonly name: string
+  readonly period: number
+  readonly entries: readonly PeriodicChoreEntry[]
+}
+
+const periodicChoresSlice = createEntitySlice<PeriodicChore>('periodic-chores')
+
+export const periodicChoresActions = periodicChoresSlice.actions
+export const selectPeriodicChores: Selector<RootState, PeriodicChore[]> = state => state.periodicChores
+
+export default periodicChoresSlice.reducer
