0390b7f4aae91e878ab52f173db49dd2d5da90bc
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-13 18:58:41 +0200

    feat(components): Add "urgency indicator" circle to PeriodicChoreBox

--- frontend/src/components/home/PeriodicChoreBox.tsx
+++ frontend/src/components/home/PeriodicChoreBox.tsx
@@ -1,5 +1,5 @@
 import './PeriodicChoreBox.css'
-import { ReactElement, useCallback, useState } from 'react'
+import { ReactElement, useCallback, useMemo, useState } from 'react'
 import ChoreBox from './ChoreBox'
 import { PeriodicChore, PeriodicChoreEntry } from '../../store/entities/periodic-chores'
 import { useEntityById } from '../../util/use-entity-by-id'
@@ -12,12 +12,32 @@ import { Link } from 'react-router-dom'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { DateTime } from 'luxon'
+import UrgencyIndicator from './UrgencyIndicator'
 
+/**
+ * After this much progress has been made towards the next due-date, show the chore as being urgent.
+ */
+const URGENCY_THRESHOLD = 0.75
+
+/**
+ * A hook that provides the most recent entry, if one exists.
+ *
+ * @param entries The chore entries.
+ * @returns The most recent entry.
+ */
 function useMostRecent (entries: readonly PeriodicChoreEntry[]): PeriodicChoreEntry | undefined {
   return entries.length > 0 ? entries[entries.length - 1] : undefined
 }
 
-function useRecentlyCompletedString (last: PeriodicChoreEntry | undefined): string {
+/**
+ * For a given chore, obtain the last completion and format it into a human-readable string.
+ *
+ * @param chore The chore.
+ * @returns The formatted string, containing member name and date of completion.
+ */
+function useRecentlyCompletedString (chore: PeriodicChore): string {
+  const last = useMostRecent(chore.entries)
+
   const lastMember = useEntityById(selectMembers, last?.memberId)
   // get only date part from ISO date-time string
   const lastDate = last != null && last.date.length >= 10 ? last.date.substring(0, 10) : '???'
@@ -25,6 +45,33 @@ function useRecentlyCompletedString (last: PeriodicChoreEntry | undefined): stri
   return last != null && lastMember != null ? `${lastMember.name}, ${lastDate}` : '--'
 }
 
+/**
+ * For a given chore, compute the urgency value (number between 0 and 1), where 0 indicates minimal urgency
+ * (i.e. the chore was very recently completed) and 1 indicates maximum urgency (the chore is due today or is
+ * already past the due date).
+ *
+ * @param chore The chore.
+ * @returns The urgency value.
+ */
+function useUrgency (chore: PeriodicChore): number {
+  const last = useMostRecent(chore.entries)
+
+  return useMemo(() => {
+    if (last == null) {
+      return 0
+    }
+    const lastDate = DateTime.fromISO(last.date, { zone: 'utc' })
+    const diffDays = Math.round(DateTime.now().diff(lastDate).as('days'))
+    return Math.max(0, Math.min(1, diffDays / chore.period))
+  }, [last, chore.period])
+}
+
+/**
+ * Perform a request to add an entry to the chore.
+ *
+ * @param chore The chore to modify.
+ * @param member The member who has completed the chore.
+ */
 async function addEntry (chore: PeriodicChore, member: Member): Promise<void> {
   await api.periodicChores.update({
     ...chore,
@@ -45,8 +92,8 @@ interface Props {
 export default function PeriodicChoreBox (props: Props): ReactElement {
   const { t } = useTranslation()
 
-  const mostRecent = useMostRecent(props.chore.entries)
-  const recentlyCompleted = useRecentlyCompletedString(mostRecent)
+  const urgency = useUrgency(props.chore)
+  const recentlyCompleted = useRecentlyCompletedString(props.chore)
 
   const [isSelectingMember, setSelectingMember] = useState(false)
 
@@ -61,8 +108,9 @@ export default function PeriodicChoreBox (props: Props): ReactElement {
   }, [props.chore])
 
   return (
-    <ChoreBox className='PeriodicChoreBox'>
+    <ChoreBox className='PeriodicChoreBox' urgent={urgency > URGENCY_THRESHOLD}>
       <div className='PeriodicChoreBox-title'>
+        <UrgencyIndicator className='PeriodicChoreBox-urgency' urgency={urgency} />
         {props.chore.name}
         <Link to={`/calendar/${props.chore._id}`} className='PeriodicChoreBox-calendar'>
           <FontAwesomeIcon icon={faCalendarAlt} />
