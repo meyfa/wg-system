718eb4f01c2211f1e5758c27b6c0486b98912e86
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-15 14:24:11 +0200

    feat(components): Implement assignment of a group via EditMemberModal
    A single group (or none) can be selected per-user via the edit modal.
    Assigning multiple groups is so far not implemented.

--- frontend/src/components/members/EditMemberModal.tsx
+++ frontend/src/components/members/EditMemberModal.tsx
@@ -8,6 +8,14 @@ import ColorPicker from './ColorPicker'
 import { useMemberEditor } from '../../editors/use-member-editor'
 import { useParametrized } from '../../util/use-parametrized'
 import BasicCheckbox from '../forms/BasicCheckbox'
+import BasicDropdown from '../forms/BasicDropdown'
+import { useAppSelector } from '../../store/store'
+import { Group, selectGroups } from '../../store/entities/groups'
+
+function useGroupFormatter (): (item: Group | undefined) => string {
+  const { t } = useTranslation()
+  return item => item == null ? t('noneOption') : item.name
+}
 
 interface Props {
   active: boolean
@@ -20,6 +28,8 @@ interface Props {
 export default function EditMemberModal (props: Props): ReactElement {
   const { t } = useTranslation()
 
+  const groups = useAppSelector(selectGroups)
+
   const editor = useMemberEditor(props.member)
   // reset editor when modal closes/opens
   useEffect(() => editor.reset(), [editor, props.active])
@@ -29,6 +39,14 @@ export default function EditMemberModal (props: Props): ReactElement {
   const doDelete = useMemo(() => {
     return props.onDelete != null && props.member != null ? props.onDelete : undefined
   }, [props.onDelete, props.member])
+
+  const groupOptions = useMemo(() => [undefined, ...groups], [groups])
+  const groupFormatter = useGroupFormatter()
+  const groupValue = useMemo(() => {
+    return editor.value.groups.length !== 0
+      ? groups.find(item => item._id === editor.value.groups[0])
+      : undefined
+  }, [groups, editor.value.groups])
 
   return (
     <EditModal title={props.member != null ? t('members.edit') : t('members.create')}
@@ -47,6 +65,14 @@ export default function EditMemberModal (props: Props): ReactElement {
         <ColorPicker
           value={editor.value.color}
           onPick={color => editor.update({ color })}
+        />
+      </FormRow>
+      <FormRow label={t('members.fields.group')}>
+        <BasicDropdown
+          options={groupOptions}
+          formatter={groupFormatter}
+          value={groupValue}
+          onSelect={group => editor.update({ groups: group != null ? [group._id] : [] })}
         />
       </FormRow>
       <FormRow label={t('members.fields.active')}>
