27c436bfa1f504f0fe42acb6cdf9ee1b943b2981
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-10-15 14:57:17 +0200

    refactor(pages): Split MembersPage into section components

---
+++ frontend/src/components/members/MembersSection.tsx
@@ -0,0 +1,41 @@
+import { ReactElement, useCallback, useMemo } from 'react'
+import { useAppSelector } from '../../store/store'
+import { Member, selectMembers } from '../../store/entities/members'
+import { EditModalRenderFn } from '../items/EditButton'
+import api from '../../api/api'
+import EditMemberModal from './EditMemberModal'
+import { useTranslation } from 'react-i18next'
+import Section from '../Section'
+import { faUser, faUserSlash } from '@fortawesome/free-solid-svg-icons'
+import Empty from '../Empty'
+import MemberItem from './MemberItem'
+
+export default function MembersSection (): ReactElement {
+  const { t } = useTranslation()
+
+  const members = useAppSelector(selectMembers)
+  const activeMembers = useMemo(() => members.filter(item => item.active), [members])
+  const inactiveMembers = useMemo(() => members.filter(item => !item.active), [members])
+
+  const renderCreateModal: EditModalRenderFn = useCallback((active, hide) => {
+    const create = async (entity: Member): Promise<void> => {
+      hide()
+      await api.members.create(entity)
+    }
+    return <EditMemberModal active={active} onSave={create} onCancel={hide} />
+  }, [])
+
+  return (
+    <>
+      <Section icon={faUser} title={t('members.active')} renderCreateModal={renderCreateModal}>
+        {activeMembers.length === 0 ? <Empty message={t('members.empty')} /> : undefined}
+        {activeMembers.map(member => <MemberItem key={member._id} member={member} />)}
+      </Section>
+      {inactiveMembers.length > 0
+        ? (<Section icon={faUserSlash} title={t('members.former')}>
+          {inactiveMembers.map(member => <MemberItem key={member._id} member={member} />)}
+        </Section>)
+        : undefined}
+    </>
+  )
+}
