4576e91778ec8e296b1a69cd2bec0d08070071a7
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-11-07 11:42:46 +0100

    feat(components): Show page reload button in ConnectionModal (#108)
    If the connection has been lost for too long, the user is offered the
    option to fully reload the page to potentially restore connectivity.

--- frontend/src/components/ConnectionModal.tsx
+++ frontend/src/components/ConnectionModal.tsx
@@ -1,4 +1,5 @@
-import { ReactElement } from 'react'
+import './ConnectionModal.css'
+import { ReactElement, useEffect, useState } from 'react'
 import Modal from './modals/Modal'
 import { useConnectionStatusDebounced } from '../util/use-connection-status'
 import { useTranslation } from 'react-i18next'
@@ -9,14 +10,38 @@ import { useTranslation } from 'react-i18next'
  */
 const ALLOWED_DISCONNECT_TIME = 3000
 
+/**
+ * The time (in milliseconds) before the page reload button is shown to the user.
+ * The button is not shown instantly, to prevent data lass when the situation would have been recoverable.
+ */
+const DELAY_BEFORE_ENABLE_RELOAD = 5000
+
+function reloadPage (): void {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload()
+}
+
 export default function ConnectionModal (): ReactElement {
   const { t } = useTranslation()
 
   const connected = useConnectionStatusDebounced(ALLOWED_DISCONNECT_TIME)
 
+  const [isReloadAllowed, setReloadAllowed] = useState(false)
+  useEffect(() => {
+    if (connected) {
+      setReloadAllowed(false)
+    } else {
+      const timeout = setTimeout(() => setReloadAllowed(true), DELAY_BEFORE_ENABLE_RELOAD)
+      return () => clearTimeout(timeout)
+    }
+  }, [connected])
+
   return (
     <Modal important active={!connected}>
       {t('lostConnection')}
+      {isReloadAllowed
+        ? <button className='ConnectionModal-reload' onClick={reloadPage}>{t('reloadPage')}</button>
+        : undefined}
     </Modal>
   )
 }
