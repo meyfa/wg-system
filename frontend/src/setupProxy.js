4850865b2005282bda899f8ee6ed712753f9f381
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-11-28 15:33:07 +0100

    chore: Simplify setupProxy.js (#133)

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,19 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-/*
- * This is only used during frontend development so that API requests are properly forwarded.
- */
+// This is only used during frontend development so that API requests are properly forwarded.
 
 module.exports = function (app) {
-  ['/api', '/websocket'].forEach(prefix => {
-    app.use(
-      prefix,
-      createProxyMiddleware({
-        // forward to the backend in Docker which is running at :8080
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        ws: true
-      })
-    )
-  })
+  app.use(createProxyMiddleware(['/api', '/websocket'], {
+    // forward to the backend in Docker which is running at :8080
+    target: 'http://localhost:8080',
+    changeOrigin: true,
+    ws: true
+  }))
 }
