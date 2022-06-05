bed13049b2265ad458e51d207cffbb4971156eb6
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-06-05 14:33:06 +0200

    feat(frontend): Manually implement circular urgency indicator (#285)
    Closes #256. Due to dependency react-circular-progressbar, the build
    was broken after upgrade to React 18. Since there hasn't been any
    progress towards react-circular-progressbar adding support for React 18,
    this patch now implements the feature manually and removes the
    dependency.

--- frontend/src/components/home/UrgencyIndicator.tsx
+++ frontend/src/components/home/UrgencyIndicator.tsx
@@ -1,10 +1,39 @@
 import './UrgencyIndicator.css'
 import { ReactElement } from 'react'
-import { buildStyles, CircularProgressbar } from 'react-circular-progressbar'
-import 'react-circular-progressbar/dist/styles.css'
 import clsx from 'clsx'
 import { faCheckCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import Icon from '../Icon'
+
+function UrgencyIndicatorCirclePath (props: {
+  stroke: string
+  size: number
+  strokeWidth: number
+  fraction: number
+}): ReactElement {
+  const { size, strokeWidth, stroke, fraction } = props
+  const inset = Math.ceil(strokeWidth / 2)
+  const radius = size / 2 - inset
+  const arcLength = Math.PI * 2 * radius
+
+  /*
+   * We draw a circle by moving to the top-center position, then drawing a half-circle to the bottom-center position,
+   * then another half-circle back to the top-center position.
+   *
+   * This circle has a stroke-dasharray that is 100% color followed by 100% transparency.
+   * By shifting the dash (via stroke-dashoffset), we can emulate a circular arc.
+   */
+
+  return (
+    <path
+      d={`M${size / 2},${inset} a${radius},${radius} 0 1 0 0,${radius * 2} a${radius},${radius} 0 1 0 0,${-radius * 2} z`}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      strokeDasharray={`${arcLength} ${arcLength}`}
+      strokeDashoffset={Math.max(0, Math.min(1, 1 - fraction)) * arcLength}
+      fill='none'
+    />
+  )
+}
 
 interface Props {
   urgency: number | boolean
@@ -22,22 +51,15 @@ export default function UrgencyIndicator (props: Props): ReactElement {
     )
   }
 
+  const size = 100
+  const strokeWidth = 25
+
   // show as green arc on top of red background
   // (higher urgency value means reduced size of green arc, revealing more of the red background)
   return (
-    <div className={clsx('UrgencyIndicator', 'circular', props.className)}>
-      <CircularProgressbar
-        value={1 - props.urgency}
-        minValue={0}
-        maxValue={1}
-        counterClockwise
-        strokeWidth={25}
-        styles={buildStyles({
-          pathColor: '#61Cd7d',
-          trailColor: '#f05353',
-          strokeLinecap: 'butt'
-        })}
-      />
-    </div>
+    <svg className={clsx('UrgencyIndicator', 'circular', props.className)} viewBox={`0 0 ${size} ${size}`}>
+      <UrgencyIndicatorCirclePath stroke='#f05353' size={size} strokeWidth={strokeWidth} fraction={1} />
+      <UrgencyIndicatorCirclePath stroke='#61Cd7d' size={size} strokeWidth={strokeWidth} fraction={1 - props.urgency} />
+    </svg>
   )
 }
