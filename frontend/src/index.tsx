63de624885e9fd25e324fc4021c2508feb3b3161
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-08-27 21:15:30 +0200

    fix: Use React 18 createRoot()

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,22 @@
+import { createRoot } from 'react-dom/client'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './i18n'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+if (container == null) {
+  throw new Error('container == null')
+}
+
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
