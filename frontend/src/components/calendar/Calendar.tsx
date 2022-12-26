fd8b277c084f849bb4f12e31e3bf4bb19041423d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-12-26 22:51:51 +0100

    fix(frontend): Use TailwindCSS for calendar components

--- frontend/src/components/calendar/Calendar.tsx
+++ frontend/src/components/calendar/Calendar.tsx
@@ -1,11 +1,11 @@
-import './Calendar.css'
-import { ReactElement, ReactNode, useCallback, useMemo, useState } from 'react'
+import { MouseEventHandler, ReactElement, ReactNode, useCallback, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { DateTime } from 'luxon'
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import Icon from '../Icon'
 import clsx from 'clsx'
 import { useCurrentDay } from '../../hooks/periodic-chores/use-current-day'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 
 export type CellRenderFn = (date: DateTime) => ReactNode
 export type CellClickHandler = (date: DateTime) => void
@@ -24,7 +24,7 @@ function CalendarHeader (): ReactElement {
     <thead>
       <tr>
         {WEEKDAYS.map(weekday => (
-          <th key={weekday} className='Calendar-th'>
+          <th key={weekday} className='pt-1 pb-2 text-xl text-center font-normal'>
             {t(`calendar.week.${weekday}`)}
           </th>
         ))}
@@ -87,16 +87,30 @@ function CalendarCell (props: { spec?: CalendarDaySpec, onClick?: CellClickHandl
 
   const today = useCurrentDay()
   const isToday = useMemo(() => spec?.date != null && spec.date.hasSame(today, 'day'), [spec, today])
-
-  if (spec == null) {
-    return <td className='Calendar-td inactive' />
-  }
+  const isInactive = spec == null
+  const isClickable = spec != null && onClick != null
 
   return (
-    <td className={clsx('Calendar-td', { clickable: onClick != null, current: isToday })} onClick={handleClick}>
-      {spec.date.day}
-      {spec.children}
+    <td className={clsx(
+      'h-16 py-1 text-center align-top border-2 border-gray-200 outline outline-2 outline-transparent -outline-offset-2',
+      isInactive && 'bg-gray-100',
+      isToday && 'outline-gray-500',
+      isClickable && 'cursor-pointer hocus:bg-[#f4f9ff] hocus:outline-[#81a4f5]'
+    )} onClick={handleClick}>
+      {spec?.date.day}
+      {spec?.children}
     </td>
+  )
+}
+
+function CalendarHeadButton (props: {
+  onClick: MouseEventHandler
+  icon: IconDefinition
+}): ReactElement {
+  return (
+    <button onClick={props.onClick} className='inline-block w-8 h-8 border-2 border-transparent rounded-full cursor-pointer outline-none hocus:border-gray-400 hocus:shadow-md'>
+      <Icon icon={props.icon} />
+    </button>
   )
 }
 
@@ -120,17 +134,15 @@ export default function Calendar (props: Props): ReactElement {
   }, [month])
 
   return (
-    <div className='Calendar'>
-      <div className='Calendar-head'>
-        <button className='Calendar-head-btn' onClick={previousMonth}>
-          <Icon icon={faAngleLeft} />
-        </button>
-        <div className='Calendar-head-month'>{monthName}</div>
-        <button className='Calendar-head-btn' onClick={nextMonth}>
-          <Icon icon={faAngleRight} />
-        </button>
+    <div className='text-base p-3 rounded bg-white shadow-md overflow-x-auto'>
+      {/* month selection */}
+      <div className='text-xl mb-4 text-center whitespace-nowrap'>
+        <CalendarHeadButton icon={faAngleLeft} onClick={previousMonth} />
+        <div className='inline-block min-w-[10em] mx-4'>{monthName}</div>
+        <CalendarHeadButton icon={faAngleRight} onClick={nextMonth} />
       </div>
-      <table className='Calendar-table'>
+      {/* calendar grid */}
+      <table className='table-fixed border-collapse w-full min-w-[36rem]'>
         <CalendarHeader />
         <CalendarBody month={month} renderCell={props.renderCell} onClickCell={props.onClickCell} />
       </table>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,11 +1,18 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant }) => {
+      // This enables e.g. `hocus:underline`, to avoid having to write `hover:underline focus:underline`.
+      addVariant('hocus', ['&:hover', '&:focus'])
+    })
+  ]
 }
