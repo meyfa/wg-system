meyfa/wg-system

commit adb9667c83971e30290966c39db94e1b9b3a9235

chore: Update ESLint configuration (#762)
Now using `@meyfa/eslint-config` v7 (supersedes #761), along with
removal of the unmaintained `eslint-config-react-app` frontend config
which is replaced by manual configuration of the required plugins.

Frontend code is fixed to adhere to the configuration's requirements,
but this results in no functional change (only code style).

frontend/.eslintrc.yml
@@ -1,27 +1,37 @@
 extends:
   - '@meyfa/eslint-config'
-  - react-app
   - plugin:react/recommended
   - plugin:react/jsx-runtime
+  - plugin:react-hooks/recommended
+
+plugins:
+  - react
+  - react-hooks
 
 parserOptions:
   project: './tsconfig.lint.json'
+  ecmaFeatures:
+    jsx: true
 
-ignorePatterns:
-  - 'src/react-app-env.d.ts'
+settings:
+  react:
+    version: 'detect'
 
 rules:
   'import/extensions': [error, never, { json: always }]
-  jsx-quotes: [error, prefer-single]
-  'react/jsx-indent': [error, 2]
-  'react/jsx-wrap-multilines': [warn, {
-    return: parens-new-line,
-    arrow: parens-new-line
-  }]
-  'react/void-dom-elements-no-children': [error]
-  'react/no-unstable-nested-components': [error]
-  'react/prop-types': [error, {
-    ignore: [children]
-  }]
-  # not supported by older Chrome versions
-  'unicorn/prefer-at': [off]
+  react/jsx-pascal-case: ['warn', { allowAllCaps: true, ignore: [] }]
+  react/no-typos: ['error']
+  react/style-prop-object: ['warn']
+  jsx-quotes: ['error', 'prefer-single']
+  react/jsx-indent: ['error', 2]
+  react/jsx-indent-props: ['error', 2]
+  react/jsx-first-prop-new-line: ['error']
+  react/jsx-closing-bracket-location: ['error']
+  react/jsx-max-props-per-line: ['error', { maximum: 1, when: 'multiline' }]
+  react/jsx-props-no-multi-spaces: ['error']
+  react/jsx-tag-spacing: ['error']
+  react/jsx-equals-spacing: ['error', 'never']
+  react/jsx-wrap-multilines: ['warn', { return: 'parens-new-line', arrow: 'parens-new-line' }]
+  react/void-dom-elements-no-children: ['error']
+  react/no-unstable-nested-components: ['error']
+  react/prop-types: ['error', { ignore: ['children'] }]

frontend/src/components/home/PeriodicChoreBox.tsx
@@ -26,7 +26,7 @@ const URGENCY_THRESHOLD = 0.75
  * @returns The formatted string, containing member name and date of completion.
  */
 function useRecentlyCompletedString (chore: PeriodicChore): string {
-  const last = chore.entries[chore.entries.length - 1]
+  const last = chore.entries.at(-1)
 
   const lastMember = useEntityById(selectMembers, last?.memberId)
   // get only date part from ISO date-time string

frontend/src/hooks/periodic-chores/use-days-since-last.ts
@@ -15,7 +15,7 @@ const UPDATE_PERIOD = ms('1 hour')
  * @returns The number of days that have passed.
  */
 export function useDaysSinceLast (chore: PeriodicChore): number | undefined {
-  const last = chore.entries[chore.entries.length - 1]
+  const last = chore.entries.at(-1)
 
   return useIntervalMemo(UPDATE_PERIOD, () => {
     if (last == null) {

frontend/src/hooks/periodic-chores/use-planned-entry.ts
@@ -62,7 +62,7 @@ function usePreferredMember (chore: PeriodicChore): Member | undefined {
  * @returns The upcoming completion information.
  */
 export function usePlannedEntry (chore: PeriodicChore): PlannedEntry | undefined {
-  const last = chore.entries[chore.entries.length - 1]
+  const last = chore.entries.at(-1)
   const member = usePreferredMember(chore)
 
   return useIntervalMemo(UPDATE_PERIOD, () => {

.eslintrc.yml
@@ -1,5 +1,3 @@
-extends:
-  - '@meyfa/eslint-config'
-
+extends: '@meyfa/eslint-config'
 parserOptions:
   project: './tsconfig.json'

backend/.eslintrc.yml
@@ -1,5 +1,3 @@
-extends:
-  - '@meyfa/eslint-config'
-
+extends: '@meyfa/eslint-config'
 parserOptions:
   project: './tsconfig.json'

frontend/src/components/calendar/Calendar.tsx
@@ -86,17 +86,20 @@ function CalendarCell (props: { spec?: CalendarDaySpec, onClick?: CellClickHandl
   }, [onClick, spec])
 
   const today = useCurrentDay()
-  const isToday = useMemo(() => spec?.date != null && spec.date.hasSame(today, 'day'), [spec, today])
+  const isToday = useMemo(() => spec?.date.hasSame(today, 'day') === true, [spec, today])
   const isInactive = spec == null
   const isClickable = spec != null && onClick != null
 
   return (
-    <td className={clsx(
-      'h-16 py-1 text-center align-top border-2 border-gray-200 outline outline-2 outline-transparent -outline-offset-2',
-      isInactive && 'bg-gray-100',
-      isToday && 'outline-gray-500',
-      isClickable && 'cursor-pointer hocus:bg-[#f4f9ff] hocus:outline-[#81a4f5]'
-    )} onClick={handleClick}>
+    <td
+      className={clsx(
+        'h-16 py-1 text-center align-top border-2 border-gray-200 outline outline-2 outline-transparent -outline-offset-2',
+        isInactive && 'bg-gray-100',
+        isToday && 'outline-gray-500',
+        isClickable && 'cursor-pointer hocus:bg-[#f4f9ff] hocus:outline-[#81a4f5]'
+      )}
+      onClick={handleClick}
+    >
       {spec?.date.day}
       {spec?.children}
     </td>

frontend/src/components/calendar/EditEntryModal.tsx
@@ -69,12 +69,14 @@ export default function EditEntryModal (props: Props): ReactElement {
   const memberValue = useEntityById(selectMembers, editor.value.memberId)
 
   return (
-    <EditModal active={props.active}
-               title={props.entry == null ? t('calendar.create') : t('calendar.edit')}
-               isValid={editor.isValid}
-               onDelete={props.onDelete}
-               onSave={save}
-               onCancel={props.onCancel}>
+    <EditModal
+      active={props.active}
+      title={props.entry == null ? t('calendar.create') : t('calendar.edit')}
+      isValid={editor.isValid}
+      onDelete={props.onDelete}
+      onSave={save}
+      onCancel={props.onCancel}
+    >
       <FormRow label={t('calendar.fields.date')}>
         <TextField disabled value={formatDate(editor.value.date)} />
       </FormRow>

commit 57c940115d5e15d68bf1304c13dbd8c7a90ed8bf

fix(frontend): Avoid using Array.prototype.at()
This was the second regression regarding `.at()` - the first one was
fixed in #612.

This particular regression was caused by #762.

To hopefully prevent a third time, an environment declaration is added
to override the relevant interface from `@types/node` such that calls
to `.at()` are not allowed.

frontend/.eslintrc.yml
@@ -35,3 +35,5 @@ rules:
   react/void-dom-elements-no-children: ['error']
   react/no-unstable-nested-components: ['error']
   react/prop-types: ['error', { ignore: ['children'] }]
+  # not supported by older Chrome versions
+  unicorn/prefer-at: ['off']

frontend/src/components/home/PeriodicChoreBox.tsx
@@ -26,7 +26,7 @@ const URGENCY_THRESHOLD = 0.75
  * @returns The formatted string, containing member name and date of completion.
  */
 function useRecentlyCompletedString (chore: PeriodicChore): string {
-  const last = chore.entries.at(-1)
+  const last = chore.entries[chore.entries.length - 1]
 
   const lastMember = useEntityById(selectMembers, last?.memberId)
   // get only date part from ISO date-time string

frontend/src/globals.d.ts
@@ -1,0 +1,5 @@
+// This is an interface from @types/node that (unfortunately) adds methods to the global scope
+// which aren't supported in the targeted ECMAScript version.
+interface RelativeIndexable {
+  at: never
+}

frontend/src/hooks/periodic-chores/use-days-since-last.ts
@@ -15,7 +15,7 @@ const UPDATE_PERIOD = ms('1 hour')
  * @returns The number of days that have passed.
  */
 export function useDaysSinceLast (chore: PeriodicChore): number | undefined {
-  const last = chore.entries.at(-1)
+  const last = chore.entries[chore.entries.length - 1]
 
   return useIntervalMemo(UPDATE_PERIOD, () => {
     if (last == null) {

frontend/src/hooks/periodic-chores/use-planned-entry.ts
@@ -62,7 +62,7 @@ function usePreferredMember (chore: PeriodicChore): Member | undefined {
  * @returns The upcoming completion information.
  */
 export function usePlannedEntry (chore: PeriodicChore): PlannedEntry | undefined {
-  const last = chore.entries.at(-1)
+  const last = chore.entries[chore.entries.length - 1]
   const member = usePreferredMember(chore)
 
   return useIntervalMemo(UPDATE_PERIOD, () => {